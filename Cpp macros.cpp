447d701f774c9ed894578c40b93938d6d8a945f9
kapoorarpit <kapoorarpit2000@gmail.com>
2021-05-05 18:38:21 +0530

    'initit'

---
+++ Cpp macros.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define forupton(i,n) for(int i=0; i<n; i++)
+#define fornto0(i,n) for(int i=n-1;i>=0;i--)
+typedef long long int ll;
+typedef std::vector<vector<int>> vecofvec;
+typedef std::vector<int> vec;
+//sum=((sum%1000000007)+1000000007)%1000000007;
+
+int main()
+{
+
+
+
+
+return 0;
+}
+
+184
